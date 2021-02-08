28853d2b2cccab2f98b7abe94bde618d0adce316
Eduardo Goncalves <eduardojnet@gmail.com>
2021-02-08 14:47:39 -0300

    Criado classe PessoaResource. Recursos para gerenciamento de Eventos e seu respectivo Listner.

--- src/main/java/com/money/resource/CategoriaResource.java
+++ src/main/java/com/money/resource/CategoriaResource.java
@@ -1,8 +1,10 @@
 package com.money.resource;
 
+import com.money.event.RecursoCriadoEvent.RecursoCriadoEvent;
 import com.money.model.Categoria;
 import com.money.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,20 +22,21 @@ public class CategoriaResource {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Categoria> listar() {
         return categoriaRepository.findAll();
     }
 
+
     @PostMapping
     public ResponseEntity<Categoria> criar(@Valid @RequestBody final Categoria categoria, final HttpServletResponse response) {
         Categoria categoriaSalva = categoriaRepository.save(categoria);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
 
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
-        response.setHeader("Location", uri.toASCIIString());
-
-        return ResponseEntity.created(uri).body(categoriaSalva);
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
     }
 
 
--- src/main/java/com/money/resource/PessoaResource.java
+++ src/main/java/com/money/resource/PessoaResource.java
@@ -1,9 +1,12 @@
 package com.money.resource;
 
+import com.money.event.RecursoCriadoEvent.RecursoCriadoEvent;
 import com.money.model.Categoria;
 import com.money.model.Pessoa;
 import com.money.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -20,6 +23,9 @@ public class PessoaResource {
     @Autowired
     private PessoaRepository pessoaRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Pessoa> listarPessoas() {
         return pessoaRepository.findAll();
@@ -28,12 +34,15 @@ public class PessoaResource {
     @PostMapping
     public ResponseEntity<Pessoa> criar(@Valid @RequestBody final Pessoa pessoa, final HttpServletResponse response) {
         Pessoa pessoaSalva = pessoaRepository.save(pessoa);
-
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-                .buildAndExpand(pessoaSalva.getCodigo()).toUri();
-        response.setHeader("Location", uri.toASCIIString());
-
-        return ResponseEntity.created(uri).body(pessoaSalva);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
     }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable final Long codigo) {
+        Pessoa pessoa = pessoaRepository.findById(codigo).orElse(null);
+        return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+    }
+
 
 }
